8a437fb151b954d6a2cece9b6146e5e39146a1ac
harri feng <harrifeng@gmail.com>
2015-05-23 12:10:39 +0000

    const is hard

---
+++ primer5/ch02/13.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[])
+{
+    double dval = 3.14;
+    ////////////////////////////////////////////////////////////////
+    // We can initialize a reference to const from any expression //
+    // that can be converted to the type of reference             //
+    ////////////////////////////////////////////////////////////////
+    const int &ri = dval;
+
+    cout << "ri is " << ri << endl;
+    return 0;
+}
+
+////////////////////////////////////////////////////
+// <===================OUTPUT===================> //
+// ri is 3                                        //
+////////////////////////////////////////////////////
