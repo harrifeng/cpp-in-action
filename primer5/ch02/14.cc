b1894a0f990023e9c080b924a51e75467e7c52d1
harri feng <harrifeng@gmail.com>
2015-05-24 08:29:55 +0000

    new example

--- primer5/ch02/14.cc
+++ primer5/ch02/14.cc
@@ -3,20 +3,26 @@
 using namespace std;
 int main(int argc, char *argv[])
 {
-    double dval = 3.14;
-    ////////////////////////////////////////////////////////////////
-    // We can initialize a reference to const from any expression //
-    // that can be converted to the type of reference             //
-    ////////////////////////////////////////////////////////////////
-    const int &ri = dval;
+    int i = 42;
+    int &r1 = i;
+    const int &r2 = i;
 
-    cout << "ri is " << ri << endl;
-    dval = 4.15;
-    cout << "ri is " << ri << endl;
+    cout << "r1 is " << r1 << " and r2 is " << r2 << endl;
+
+    r1 = 0;
+    cout << "r1 is " << r1 << " and r2 is " << r2 << endl;
+
+    ///////////////////////////////////////////////////////////////
+    // 14.cc:15:8: error: assignment of read-only reference 'r2' //
+    //  r2 = 23;                                                 //
+    //     ^                                                     //
+    ///////////////////////////////////////////////////////////////
+    // r2 = 23;
     return 0;
 }
 
 ////////////////////////////////////////////////////
 // <===================OUTPUT===================> //
-// ri is 3                                        //
+// r1 is 42 and r2 is 42                          //
+// r1 is 0 and r2 is 0                            //
 ////////////////////////////////////////////////////
